390cb4a593584e2120acc7f065702370c73dadb2
dnkt182@gmail.com <dnkt182@gmail.com>
2021-08-21 09:44:19 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <Tutorial/>
+  <nuxt-link to="/fun">Go to Fun</nuxt-link>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
